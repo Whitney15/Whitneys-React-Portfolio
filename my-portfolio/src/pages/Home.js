5cb9e3e51b64b70a705c13de074e981757f0dc6f
Whitney15 <114629880+Whitney15@users.noreply.github.com>
2023-07-31 06:48:51 -0500

    added git hub pages install

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -5,21 +5,21 @@ import HeroImage from '../assets/hero.jpg'
 const Home = () => {
   return <div name="home" className=' w-full text-white'>
             <div className='max-w-screen-lg mx-auto flex flex-col  justify-center h-full px-12 md:flex-row mt-16 pb-56'>
-                <div>
+                <div className='mr-4 mb-4'>
                     <p className=' text-pink-500 text-6xl font-bold'>Hi im Whitney</p>
                     <p className='text-3xl'>Im a front-end developer and Ui Designer</p>
                 
-                    <div>
+                    {/* <div>
                         <button className=' bg-lime-300 text-black px-6 py-3 my-8 mx-auto rounded-md hover:scale-110 duration-300'>Portfolio
                         </button>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div>
             <img 
             src={HeroImage} 
             alt='my profile' 
-            className="inline rounded-3xl mx-auto h-64"
+            className="inline rounded-3xl mx-auto h-64 "
             /> 
         </div>   
             </div>
